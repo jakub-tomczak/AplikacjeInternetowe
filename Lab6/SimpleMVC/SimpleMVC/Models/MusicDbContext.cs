55864ade8f5550b20dddaf7a7d449b5e53ddac7b
Jakub Tomczak <jakub.tomczak@live.com>
2018-04-28 00:54:40 +0200

    Add simple mvc in asp .net project with code first database.

---
+++ Lab6/SimpleMVC/SimpleMVC/Models/MusicDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SimpleMVC.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext() : base("DefaultConnection") { }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
---
+++ Lab6/SimpleMVC/SimpleMVC/Models/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMVC.Models
+{
+    public class Song
+    {
+        public string Name { get => name; set => name = value; }
+        public string Artist { get => artist; set => artist = value; }
+        public string Genre { get => genre; set => genre = value; }
+        [Key]
+        public int Id { get => id; set => id = value; }
+
+        private string name;
+        private string artist;
+        private string genre;
+        private int id;
+    }
+}
